a52ad8a72862d452fa9daecb884fcfad1276753d
Freebird <bo.salinas12@gmail.com>
2023-08-27 16:04:40 -0500

    added thoughts routes and get all thoughts in thoughtscontroller

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -3,13 +3,15 @@ const {
     getUsers,
     getOneUser,
     createUser,
-    // deleteUser
+    deleteUser,
+    updateThought
 } = require('../../controllers/userController');
 
 // api/users
 router.route('/').get(getUsers).post(createUser);
 // api/users/:id
-router.route('/:userId').get(getOneUser)
-// .delete(deleteUser);
+router.route('/:userId').get(getOneUser).delete(deleteUser);
+
+router.route('/:userId/thoughts').post(updateThought);
 
 module.exports = router;
